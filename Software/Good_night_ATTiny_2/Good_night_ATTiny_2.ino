248d1f550589c83b2bee4c43940879cf910cb489
Greg Goldman <ghg3@duke.edu>
2018-12-05 13:03:25 -0500

    final code and mechanical!
    Still need to clean up some folder structure

--- Software/Good_night_ATTiny_2/Good_night_ATTiny_2.ino
+++ Software/Good_night_ATTiny_2/Good_night_ATTiny_2.ino
@@ -10,6 +10,7 @@ int LEDPIN = 4;
 int BUTTONPIN = 3;
 bool Flash = LOW;
 volatile int State = 0;
+volatile int lastState = 4;
 /*
    0: off
    1: low
@@ -18,16 +19,18 @@ volatile int State = 0;
    4: 2Hz blink at high intensity
 */
 
-unsigned long t1 = 0; //current time
-unsigned long t2 = 0; // previous time
+unsigned long bounceTime = 200000;
+volatile unsigned long t1 = 0; //current time
+volatile unsigned long t2 = 0; // previous time
+volatile unsigned long lastPressedTime = 0;
 
 void setup() {
-    // sets PB4 to output, all others to inputs
+  // sets PB4 to output, all others to inputs
   DDRB  = 0b00010000;
 
   //disable pullup on PB3 and PB4
   PORTB = 0b11100111;
-  
+
   // set all outputs to low first (default, likely unecessary)
   PINB = 0b00000000;
 
@@ -38,35 +41,58 @@ void setup() {
   // globally stage change interrupts to be enabled (still requires sei())
   GIMSK = 0b00100000;
 
-  ADCSRA &= ~_BV(ADEN);                   // ADC off
-
+  ADCSRA &= ~_BV(ADEN);  // ADC off
 
   //enable interrupts after setup
   sei();
+
 }
 
 void loop() {
-  t1 = millis();
+
+  /*
+     Because the timer resets whenever the attiny85 goes to sleep,
+     I can't use the time difference to debounce from state 0
+
+     The common case is that I'll press the button on State 4 (flash),
+     and it will skip from zero (sleep) to 1 (low)
+
+     It almost always only bounces once, which must be happening during
+     the switch tree, so it jumps from State = 4 to State = 1;
+     This jump from 4->0->1 must happen before another loop() completes.
+     I know that the ATTiny85 is not getting to State = 0, going to sleep,
+     and then waking back up on the bounce because I have a delayMicroseconds
+     line inside of that case which should ignore that bounce.
+
+     If I keep track of the last State called at the top of every loop,
+     I'll know if the State has moved by more than 1 in the course of a
+     single loop, which is the shortest amount of time for detection.
+  */
   switch (State) {
     case 0:
       analogWrite(LEDPIN, 0);
-
+      delayMicroseconds(bounceTime);
       set_sleep_mode(SLEEP_MODE_PWR_DOWN);
       sleep_enable();
       sleep_cpu();
       break;
     case 1:
-      analogWrite(LEDPIN, 40);
       sleep_disable();
+      if (lastState == 4){
+        State = 0;
+        break;
+      }
+      analogWrite(LEDPIN, 20);
       break;
     case 2:
-      analogWrite(LEDPIN, 120);
+      analogWrite(LEDPIN, 100);
       break;
     case 3:
       analogWrite(LEDPIN, 255);
       break;
     case 4:
-      if (t1 - t2 > 250) {
+      t1 = micros();
+      if (t1 - t2 > 250000) {
         Flash = !Flash;
         t2 = t1;
       }
@@ -81,16 +107,31 @@ void loop() {
       analogWrite(LEDPIN, 0);
       break;
   }
+  lastState = State;
 }
 
 //Pin Change Interrupt Request 0 (don't look at nested interrupts)
 ISR(PCINT0_vect) {
   // This will trigger on both rising and falling by default
   // need to read the state of the pin to tell if rising
+  t1 = micros();
   if (digitalRead(BUTTONPIN)) {
-    State = State + 1;
-    if (State > 4) {
-      State = 0;
+    if (t1 - lastPressedTime > bounceTime || State == 0) {
+      State = State + 1;
+      if (State > 4) {
+        State = 0;
+      }
     }
   }
+  lastPressedTime = micros();
 }
+
+
+
+void incrementState(){
+  State+=1;
+  if (State > 4){
+    State = 0;
+  }
+}
+
---
+++ Software/Good_night_ATTiny_3/Good_night_ATTiny_3.ino
@@ -0,0 +1,142 @@
+/*
+ * FinalFlashlightCode.ino
+ * Greg Goldman
+ * BME 590: Medical Instrumentation Design
+ * Fall, 2018
+ * 
+ * This code operates a flashlight on an ATTiny85 processor at a clock speed of 8 MHz
+ * It is capable of 5 modes: low, medium, high, flash at high, and off
+ * When off, the processor is put to sleep, reducing its current draw from a few mA
+ * to less than 50 uA.
+ * 
+ * It uses interrupts to record button inputs, and it has rudimentary software debounce 
+ * built in.
+ * 
+ * A great challenge with the debounce was that the timer resets after going to sleep, so
+ * it was hard to tell the time since the last press if the device had slept. This was solved
+ * by implementing a delay of the bounce wait time before going to sleeep. 
+ * 
+ * All time variables are in microseconds because micros() works in the ISR, but millis doesn't
+ * 
+ * In an unsigned long, there are 2^32 bits, so I can count to about 4.295 billion microseconds,
+ * or about 4295 seconds, which is about 2.98 days before the timer count can roll over
+ * 
+ * Related content can be found at https://github.com/GregGoldman/BlinkLight
+ */
+
+//Includes
+#include <avr/io.h> 
+#include <avr/interrupt.h> //https://www.nongnu.org/avr-libc/user-manual/group__avr__interrupts.html
+#include <avr/sleep.h> //https://www.nongnu.org/avr-libc/user-manual/group__avr__sleep.html
+//https://bigdanzblog.wordpress.com/2014/08/10/attiny85-wake-from-sleep-on-pin-state-change-code-example/
+// how to put attiny to sleep
+
+int LEDPIN = 4;
+int BUTTONPIN = 3;
+bool Flash = LOW;
+volatile int State = 0;
+
+
+volatile int pressCount = 0;
+/*
+   0: off
+   1: low
+   2: med
+   3: high
+   4: 2Hz blink at high intensity
+*/
+
+///online sources suggest to declare variables which go in the ISR as volatile
+unsigned long bounceTime = 200000; // how long to wait to avoid bounce: 200 ms
+volatile unsigned long now = 0; //current time
+unsigned long lastFlashTime = 0; // previous time
+volatile unsigned long lastPressedTime = 0;
+
+void setup() {
+  // sets PB4 to output, all others to inputs
+  DDRB  = 0b00010000;
+
+  //disable pullup on PB3 and PB4
+  PORTB = 0b11100111;
+
+  // set all outputs to low first (default, likely unecessary)
+  PINB = 0b00000000;
+
+  //disable interrupts during setup
+  cli();
+  // choose PB 3 for your interrupt pin
+  PCMSK = 0b00001000;
+  // globally stage change interrupts to be enabled (still requires sei())
+  GIMSK = 0b00100000;
+
+  ADCSRA &= ~_BV(ADEN);  // ADC off
+
+  //enable interrupts after setup
+  sei();
+}
+
+void loop() {
+  // only increment the State at the top of the loop so it can't change during 
+  // the Switch Tree or other inconvenient places
+  if (pressCount > 0) {
+    State += 1;
+    if (State > 4) {
+      State = 0;
+    }
+    pressCount = 0;
+  }
+  
+  switch (State) {
+    case 0:
+      analogWrite(LEDPIN, 0);
+      // the longest accurate microsecond delay is 16383 according to arduino.cc
+      // as such, use delay() instead
+      // this is one of the rare cases we are OK with a delay.
+      delay(bounceTime/1000);
+      set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+      sleep_enable();
+      sleep_cpu();
+      break;
+    case 1:
+      sleep_disable(); // not entirely sure this line is needed
+      analogWrite(LEDPIN, 20);
+      break;
+    case 2:
+      analogWrite(LEDPIN, 100);
+      break;
+    case 3:
+      analogWrite(LEDPIN, 255);
+      break;
+    case 4:
+      now = micros();
+      if (now - lastFlashTime > 250000) {
+        Flash = !Flash;
+        lastFlashTime = now;
+      }
+      if (Flash) {
+        analogWrite(LEDPIN, 255);
+      }
+      else {
+        analogWrite(LEDPIN, 0);
+      }
+      break;
+    default:
+      analogWrite(LEDPIN, 0);
+      break;
+  }
+}
+
+//Pin Change Interrupt Request 0 (don't look at nested interrupts)
+ISR(PCINT0_vect) {
+  // This will trigger on both rising and falling by default
+  // need to read the state of the pin to tell if rising
+  now = micros();
+  if (digitalRead(BUTTONPIN)) {
+    if (now - lastPressedTime > bounceTime || State == 0) {
+      pressCount += 1;
+    }
+  }
+  lastPressedTime = micros();
+}
+
+
